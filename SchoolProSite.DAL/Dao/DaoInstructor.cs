6803d2124aed8ed036f3a3d19a1b421ac57e91a3
EzeQuezada <20231021.itla.edu.do>
2024-03-26 23:10:04 -0400

    Instructor completado

---
+++ SchoolProSite.DAL/Dao/DaoInstructor.cs
@@ -0,0 +1,51 @@
+﻿using SchoolProSite.DAL.Context;
+using SchoolProSite.DAL.Entities;
+using SchoolProSite.DAL.Interfaces;
+
+
+namespace SchoolProSite.DAL.Dao
+{
+    internal class DaoInstructor : IDaoInstructor
+    {
+        private readonly SchoolContext context;
+
+        public DaoInstructor(SchoolContext context)
+        {
+            this.context = context;
+        }
+        public bool ExistsInstructor(Func<Instructor, bool> filter)
+        {
+            ;
+        }
+
+        public Instructor GetInstructor(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Instructor> GetInstructors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Instructor> GetInstructors(Func<Instructor, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveInstructor(Instructor instructor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveInstructor(Instructor instructor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateInstructor(Instructor instructor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
